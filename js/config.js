813fc54dd406c4962e03c97d462b701137df221e
mawani <44065097+parkmawani@users.noreply.github.com>
2023-11-18 03:29:03 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ var Config = {};
  * What should the text in the center of the screen be?
  * if empty it will fill in your Server Name
  */
-Config.title = "";
+Config.title = "마와니의 평전서버";
 
 /**
  * Enable map text in the top left corner of the screen?
@@ -27,8 +27,8 @@ Config.enableAnnouncements = true;
  * only works if enableAnnouncements = true
  */
 Config.announceMessages = [
-  "Enjoy your stay!",
-  "This loadingscreen is made by Exsite"
+  "여긴 어디",
+  "난 누구"
 ];
 
 /**
